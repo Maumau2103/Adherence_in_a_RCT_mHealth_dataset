8c4d0cc4d5a095b053eb51e2a3685abdf6f48566
danielbrkic00 <daniel.brkic@st.ovgu.de>
2023-05-16 17:20:40 +0200

    task5 first method

---
+++ code/task5_adherence_level.py
@@ -0,0 +1,17 @@
+from task1_phases import *
+
+
+def get_user_adh_level(df_sorted, user_id, start_day=None, end_day=None):
+
+    # get timeline array from Task 1
+    timeline = get_user_timeline(df_sorted, user_id, start_day, end_day, 'collected_at')
+
+    # create adherence level variable
+    adh_level = float(0)
+    # calculate adherence level for user in timeline
+    for index in timeline:
+        adh_level += index
+
+    adh_level = adh_level / len(timeline)
+
+    return adh_level
