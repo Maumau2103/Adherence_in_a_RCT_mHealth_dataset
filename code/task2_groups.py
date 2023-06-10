7d9bfa9de9e57c419aa78ef9c9665975b867d18d
LucyBas <lucrezi.bastisch@st.ovgu.de>
2023-06-10 17:11:42 +0200

    task 2

--- code/task2_groups.py
+++ code/task2_groups.py
@@ -1,21 +1,15 @@
 import numpy as np
 from sklearn.cluster import KMeans
-from helper import *
-from task1_phases import get_user_timeline
+
+from task1_phases import get_user_timeline, get_all_user_timelines
 from task5_adherence_level import get_user_adh_percentage
 
 
-def cluster_timelines(df_sorted, num_clusters=3, start_day=None, end_day=None):
-    all_timelines = []
-
-    # Iterate over each unique user ID
-    for user_id in df_sorted[s_table_key].unique():
-        # Get the user timeline using the existing function
-        timeline = get_user_timeline(df_sorted, user_id, start_day, end_day, s_table_sort_by)
-        all_timelines.append(timeline)
+def cluster_timelines(df_sorted, num_clusters=3, column_name='collected_at', start_day=None, end_day=None):
+    timelines = get_all_user_timelines(df_sorted, start_day, end_day)
 
     # Convert the timelines to a NumPy array
-    timelines_data = np.array(all_timelines)
+    timelines_data = np.array(timelines)
 
     # Initialize and train K-Means model
     kmeans = KMeans(n_clusters=num_clusters)
@@ -31,7 +25,7 @@ def cluster_adherence_levels(df_sorted, num_clusters=3, start_day=None, end_day=
     all_adh_levels = []
 
     # Iterate over each unique user ID
-    for user_id in df_sorted[s_table_key].unique():
+    for user_id in df_sorted['user_id'].unique():
         # Get the adherence level for each user using the existing function
         adh_level = get_user_adh_percentage(df_sorted, user_id, start_day, end_day)
         all_adh_levels.append(adh_level)
@@ -47,3 +41,24 @@ def cluster_adherence_levels(df_sorted, num_clusters=3, start_day=None, end_day=
     adherence_cluster_labels = kmeans.labels_
 
     return adherence_cluster_labels
+
+
+def cluster_note_timelines(df_sorted, num_clusters=4, column_name='collected_at', start_day=None, end_day=None):
+    timelines = get_all_user_timelines(df_sorted, start_day, end_day, column_name)
+    notes = get_all_user_timelines(df_sorted, start_day, end_day, 'value_diary_q11')
+
+    # Convert the timelines and notes to NumPy arrays
+    timelines_data = np.array(timelines)
+    notes_data = np.array(notes)
+
+    # Create pairs of binary arrays (timelines and notes)
+    paired_data = np.column_stack((timelines_data, notes_data))
+
+    # Initialize and train K-Means model
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(paired_data)
+
+    # Cluster labels for the paired data
+    timeline_notes_cluster_labels = kmeans.labels_
+
+    return timeline_notes_cluster_labels
