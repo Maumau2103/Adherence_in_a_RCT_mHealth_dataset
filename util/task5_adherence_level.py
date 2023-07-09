5c07977cf0544471dbd878daa5d682c72edcd3f1
danielbrkic00 <daniel.brkic@st.ovgu.de>
2023-07-10 01:34:41 +0200

    task5 new method added

--- util/task5_adherence_level.py
+++ util/task5_adherence_level.py
@@ -15,3 +15,22 @@ def get_user_adh_percentage(df_sorted, user_id, start_day=None, end_day=None, co
     adh_percentage = adh_percentage / len(timeline) # percentage
 
     return adh_percentage
+
+def get_user_adh_level(adh_level, full_adh_threshold=80, non_adh_threshold=40, start_day=None, end_day=None) : # adh_level of 1=non-adherent, 2=partial, 3=full
+    adherence_group = []
+
+    for user_id in df_sorted['user_id'].unique():
+        if adh_level == 1:
+            adh_percentage = get_user_adh_percentage(df_sorted, user_id, start_day, end_day)
+            if adh_percentage < non_adh_threshold:
+                adherence_group.append(adh_percentage)
+        elif adh_level == 2:
+            adh_percentage = get_user_adh_percentage(df_sorted, user_id, start_day, end_day)
+            if adh_percentage >= non_adh_threshold and adh_percentage < full_adh_threshold:
+                adherence_group.append(adh_percentage)
+        else:
+            adh_percentage = get_user_adh_percentage(df_sorted, user_id, start_day, end_day)
+            if adh_percentage >= full_adh_threshold:
+                adherence_group.append(adh_percentage)
+
+    return adherence_group
