52ad9d75055e9c90889ac8d3fae764dcd716ef97
PatrickBautz <patrick.bautz@ovgu.de>
2023-07-26 19:43:54 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	notebook/Softwareprojekt - Jupyter Notebook.ipynb

--- util/test_file_task2.py
+++ util/test_file_task2.py
@@ -1,5 +1,7 @@
 from task2_groups import *
+from helper import *
 import pandas as pd
 
-df_sorted = pd.read_csv(r'C:\Users\User\PycharmProjects\Adherence_in_a_RCT_mHealth_dataset\data\dataset_sorted.csv')
+df_map = find_path(s_file_name)
+df_sorted = group_and_sort(df_map)
 test_cluster_timelines = cluster_timelines(df_sorted, num_clusters=3)
