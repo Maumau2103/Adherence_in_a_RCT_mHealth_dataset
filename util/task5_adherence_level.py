5c759212dcb9cb93a6d762969dbf020adafa7ea7
danielbrkic00 <daniel.brkic@st.ovgu.de>
2023-07-28 22:49:31 +0200

    task 5 added comments to last method

--- util/task5_adherence_level.py
+++ util/task5_adherence_level.py
@@ -39,10 +39,12 @@ def get_user_adh_level(df_sorted, adh_level, full_adh_threshold=80, non_adh_thre
     return adherence_group
 
 def get_user_adh_level_cluster(df_sorted, adh_level, start_day=s_start_day, end_day=s_end_day):
+    # adh_level of 1 = low adherence, 2 = moderate, 3 = high
     cluster_levels = cluster_adherence_levels(df_sorted, 3, start_day, end_day)
     adherence_group = []
     first_three_unique_clusters = []
     saving_indices = []
+    # put the first three unique instances of clusters(1,2,3) in an array and save their respective indices(user_ids)
     for i in range(len(cluster_levels)):
         if len(first_three_unique_clusters) == 3:
             break
@@ -52,9 +54,10 @@ def get_user_adh_level_cluster(df_sorted, adh_level, start_day=s_start_day, end_
         saving_indices.append(i)
 
     adh_percentages = []
+    # calculate the adh_percentage of the saved indices(user_ids)
     for i in saving_indices:
         adh_percentages.append(get_user_adh_percentage(df_sorted, i, start_day, end_day))
-
+    # order the first three unique clusters based on their respective adh_percentage going from lowest to highest
     value_at_0 = first_three_unique_clusters[adh_percentages.index(min(adh_percentages))]
     value_at_2 = first_three_unique_clusters[adh_percentages.index(max(adh_percentages))]
     value_at_1 = 0
@@ -65,6 +68,8 @@ def get_user_adh_level_cluster(df_sorted, adh_level, start_day=s_start_day, end_
             break
 
     sorted_clusters = [value_at_0, value_at_1, value_at_2]
+    # now we now what clusters is the lowest adh_level and what is the highest
+    # put all users from a specific cluster in an array based on which adh_level the user wants to have
     for j in range(len(cluster_levels)):
         if cluster_levels[j] == sorted_clusters[adh_level-1]:
             adherence_group.append(j)
